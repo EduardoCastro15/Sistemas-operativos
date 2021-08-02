65c5610ed0f6859e7819cd22aae760d7189ddf38
EduardoCastro15 <67349735+EduardoCastro15@users.noreply.github.com>
2021-08-01 21:47:55 -0500

    Add files via upload

---
+++ ClasesVic/UsodePipes.c
@@ -0,0 +1,40 @@
+//uso de pipes
+#include<sys/wait.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+int main(){
+	int numero; //numero a analizar
+  int pid; //pid del proceso hijo
+	int pipedes[2]; //Descriptor de la tubería
+
+	if(pipe(pipedes)==-1){//cierra el programa en caso de que no se haya creado el pipe
+		perror("Error al crear el pipe\n");
+		exit(0);
+	}
+
+	pid=fork();
+	
+  if(pid==0){//proceso hijo
+    wait(NULL);
+    close(pipedes[1]);
+    read(pipedes[0],&numero,sizeof(numero));
+    
+    if(numero%2==0)
+      printf("El número ingresado: %d, es un numero par\n",numero );
+    else{
+      printf("El número ingresado: %d, es impar\n",numero );
+    }
+    
+    printf("Ya terminé padre\n");
+
+	}
+	else{//proceso padre
+    close(pipedes[0]);
+    puts("ingresa un número entero: \n");
+    scanf("%d",&numero);
+    write(pipedes[1],&numero,sizeof(numero));
+	}	
+return 0;
+}
